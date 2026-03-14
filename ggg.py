d4d37cc6c06eb3004c132b9708c182f431cc0448
thunderopg7234-pixel <thunderopg7234@gmail.com>
2026-03-15 02:06:04 +0800

    Update ggg.py

--- ggg.py
+++ ggg.py
@@ -51,6 +51,8 @@ print("--- DEBUG: STEP 3 (Connecting to Telegram) ---")
 # Initialize Telethon client withouit string session
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
+print("--- DEBUG: STEP 4 (Bot is LIVE!) ---"
+
 from telethon import TelegramClient, events
 from telethon.tl.functions.contacts import GetContactsRequest, DeleteContactsRequest
 import asyncio
